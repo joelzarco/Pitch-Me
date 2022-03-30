b8c145758f765975c665f5c5e9e0d47e5d427067
joelzarco <joelzarco@me.com>
2022-03-30 16:19:35 -0600

    Initial commit-No Storyboards

---
+++ PitchMe-Parfait/PitchMe-Parfait/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  PitchMe-Parfait
+//  Refactored version of Udacity's free iOS course app PitchMe Perfect
+//  Created by Johel Zarco on 30/03/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application : UIApplication, didFinishLaunchingWithOptions launchOptions : [UIApplication.LaunchOptionsKey : Any]?) -> Bool{
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemPink
+        window?.rootViewController = ViewController()
+        return true
+    }
+}
+
---
+++ PitchMe-Parfait/PitchMe-Parfait/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PitchMe-Parfait
+//
+//  Created by Johel Zarco on 30/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
